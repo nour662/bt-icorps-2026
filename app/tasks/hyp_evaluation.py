600b0e3e60c66ac5812db14f76c8d0feb4ba642c
KasraSedghi <sedghik07@gmail.com>
2026-01-07 16:34:28 -0500

    importing the langchain libraries in

---
+++ app/tasks/hyp_evaluation.py
@@ -0,0 +1,5 @@
+import os
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
