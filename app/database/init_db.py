491d0031eaceac99e3f7771b2a21f5b83fc9405e
czhu1231 <clairezhu05@gmail.com>
2026-01-08 13:17:35 -0500

    process/store chunks and test interaction with docker database

---
+++ app/database/init_db.py
@@ -0,0 +1,30 @@
+import sys
+from pathlib import Path
+from sqlalchemy import text
+
+# Setup paths
+root = Path(__file__).resolve().parent.parent.parent
+if str(root) not in sys.path:
+    sys.path.insert(0, str(root))
+    
+from app.core.database import engine, Base
+# Ensure models are imported so Base knows about them
+from app.models.document_chunk_table import DocumentChunk 
+
+def create_tables():
+    print(f"Connecting to: {engine.url}")
+    try:
+        # 1. Test Connection
+        with engine.connect() as conn:
+            conn.execute(text("SELECT 1"))
+            print("Connection successful!")
+
+        # 2. Create Tables
+        Base.metadata.create_all(bind=engine)
+        print("SQLAlchemy 'create_all' command sent.")
+        
+    except Exception as e:
+        print(f"Error occurred: {e}")
+
+if __name__ == "__main__":
+    create_tables()
---
+++ app/database/process_chunks.py
@@ -0,0 +1,82 @@
+import os
+from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv #dotenv to read from env
+from sqlalchemy.orm import Session
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from app.models.document_chunk_table import DocumentChunk 
+
+def process_chunks(db: Session, interview_id: int):
+    file = "src/test_file/Chat w_ Bobby.docx.pdf" # hardcode file to test :P
+    #interview_id = 2
+
+    all_chunks = process_file_to_chunks(file)
+    all_vectors = process_chunks_to_vectors(all_chunks)
+
+    chunk_objects = []
+    for chunk, vector in zip(all_chunks, all_vectors):
+        # use zip to pair all_chunks and all_vectors 
+        obj = DocumentChunk(
+            content=chunk.page_content,
+            embedding=vector,       
+            # interview_id=interview_id,
+            s3_key=None
+        )
+        chunk_objects.append(obj)
+
+
+    # save to database
+    try:
+        db.add_all(chunk_objects)
+        db.commit()
+        print(f"Stored {len(chunk_objects)} text chunks for interview.")
+    except Exception as e:
+        # error checking
+        db.rollback() # if error, remove all chunks added to database
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    # main for testing, print output to see if working
+    print("--- STEP 1: Main block reached ---", flush=True)
+    try:
+        from app.core.database import SessionLocal
+        db = SessionLocal()
+        print("--- STEP 2: DB Session created ---", flush=True)
+        process_chunks(db, interview_id=1)
+    except Exception as e:
+        print(f"CRITICAL ERROR: {e}", flush=True)
+    db = SessionLocal()
+
+
+def process_file_to_chunks(file):
+    # compatibility with various file types
+    file_type = os.path.splitext(file)[1].lower() # determine file type
+
+    if file_type == ".pdf":
+        loader = PyPDFLoader(file)
+    elif file_type == ".docx" or file_type == ".doc":
+        loader = Docx2txtLoader(file)
+    pages = loader.load()
+
+    # chunk text based on paragraph, then sentence, then character
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=600, # determine optimal chunk size later
+        chunk_overlap=100, # preserves context if awkward split
+        separators=["\n\n", "\n", ".", " ", ""]
+    )
+
+    # split documents
+    chunks = text_splitter.split_documents(pages)
+    return chunks
+
+def process_chunks_to_vectors(chunks):
+    # will require openai key, skeleton for now
+    embeddings_model = OpenAIEmbeddings(model="text-embedding-3-small") 
+
+    # loader creates list of document objects which contain page_content and metadata
+    texts = [chunk.page_content for chunk in chunks] # extract text from chunks
+
+    # generate vectors via openai -> 1536 numbers per chunk
+    vectors = embeddings_model.embed_documents(texts)
+
+    return vectors
