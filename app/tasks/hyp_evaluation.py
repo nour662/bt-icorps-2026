4eaf08f95306758ead01ca6c91792858cb859004
KasraSedghi <sedghik07@gmail.com>
2026-01-08 17:01:03 -0500

    Added rag and celeary route just need to double check data base vecortization and add keys

--- app/tasks/hyp_evaluation.py
+++ app/tasks/hyp_evaluation.py
@@ -3,6 +3,8 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts.chat import ChatPromptTemplate
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
+from langchain_core.runnables import RunnablePassthrough 
+from langchain_core.output_parsers import StrOutputParser
 
 #apparently this is the correct import path for vector store 
 # when using langchain but it might be sequal algemy not sure DOUBLE CHECK 
@@ -11,8 +13,8 @@ from langchain_community.vectorstores import PGVector
 #importing settings and prompt template
 from app.core.config import settings
 
-#not sure if this is the correct import path DOUBLE CHECK
-from app.core import EVALUATION_PROMPT_TEMPLATE
+#imports file from system prompts
+from app.systemprompts.hyp_evaluation_prompt import EVALUATION_PROMPT
 
 #Step 1 loading data
 loader = PyPDFLoader("PDF file path here")
@@ -25,35 +27,38 @@ rc_splitter = RecursiveCharacterTextSplitter(
     )
 docs = rc_splitter.split_documents(data)
 
-#step 3 creating embeddings and retrieval
+#step 3 creating embeddings, vecorization, and retrieval
 embeddings = OpenAIEmbeddings(
     api_key="The API Key for Embeddings here", model="text-embedding-3-small"
     )
 
 vector_store = PGVector.from_documents(
     docs, embeddings,
+
     #not sure if I need these last 2 parameters DOUBLE CHECK
     collection_name="hypothesis_guidelines",
     connection_string=settings.DATABASE_URL
     )
 retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k":4})
-#step 4 building a prompt template for evaluation
 
-prompt_template = ChatPromptTemplate.from_messages([
-    ("system", system_instruction),
-    ("user", "Evaluate the following hypothesis: {hypothesis}")
-])
-system_instruction = 
+# Step 4: Chaining Components Together
 
-#step 5 chaining it all together NOT DONE YET
+def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
 
-rag_chain = ({
-    "guidelines": retriever, 
-})
+# This combines the Retrieval + The Prompt from File 1 + The LLM
+rag_chain = (
+    {
+    "guidelines": retriever | format_docs, 
+    "hypothesis": RunnablePassthrough()     
+    }
+    | EVALUATION_PROMPT                 # Uses the imported template
+    | ChatOpenAI(model="gpt-4o", api_key=settings.OPENAI_API_KEY)
+    | StrOutputParser()
+)
 
 #celery route for tasks evaluate_hypothesis_task
 # --- CELERY TASK ---
 def evaluate_hypothesis_task(user_hypothesis: str):
-    chain = get_rag_chain()
-    result = chain.invoke(user_hypothesis)
+    result = rag_chain.invoke(user_hypothesis)
     return result
