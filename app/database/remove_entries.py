dc948cda5409bf0f27b828d0fd0d4286fac5ec54
czhu1231 <clairezhu05@gmail.com>
2026-01-12 16:20:08 -0500

    remove entries in the database

--- app/database/remove_entries.py
+++ app/database/remove_entries.py
@@ -1,5 +1,22 @@
 from sqlalchemy.orm import Session
 from app.models.hypotheses_table import Hypotheses
+from app.models.team_table import Team
+from app.models.interviews_table import Interviews
+
+def delete_team(db: Session, team_id:str):
+    try:
+        # relationships are set up using cascade so once a team is deleted, all associated info is also deleted
+        record = db.query(Team).filter(Team.id == team_id).first() 
+        if record:
+            db.delete(record)
+            db.commit()
+            return True
+        else:
+            return False
+        
+    except Exception as e:
+        db.rollback()
+        return None
 
 def delete_hypothesis(db: Session, hypothesis_id: int):
     try:
@@ -11,6 +28,19 @@ def delete_hypothesis(db: Session, hypothesis_id: int):
         else:
             return False
     
+    except Exception as e:
+        db.rollback()
+        return None
+    
+def delete_interview(db: Session, interview_id: int):
+    try:
+        record = db.query(Interviews).filter(Interviews.id == interview_id).first()
+        if record:
+            db.delete(record)
+            db.commit()
+            return True
+        else:
+            return False
     except Exception as e:
         db.rollback()
         return None
