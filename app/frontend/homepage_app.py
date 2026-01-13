7d4c1478f50d1e92e65ef9e14b27a08d55f13f1e
KasraSedghi <sedghik07@gmail.com>
2026-01-13 17:00:22 -0500

    created a folder for the frontend

---
+++ app/frontend/homepage_app.py
@@ -0,0 +1,10 @@
+import streamlit as st 
+import plotly.express as px
+#to run the file to do: streamlit run app/frontend/homepage_app.py
+
+st.title("Project Dashboard")
+st.write("Select a module to begin:")
+
+col1, col2, col3 = st.columns(3)
+
+
