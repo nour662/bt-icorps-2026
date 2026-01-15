cde329cece80a17f3fe21de961296ed07e0c83e8
KasraSedghi <sedghik07@gmail.com>
2026-01-15 14:10:42 -0500

    finished the inital front end parts of page 1

--- frontend/pages/1_Page1.py
+++ frontend/pages/1_Page1.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import Session
 from theme import i_corp_theme, sidebar
 from css import apply_css
 import streamlit as st
-from backend.app.worker.hyp_evaluation import evaluate_hypothesis_task
+# from backend.app.worker.hyp_evaluation import evaluate_hypothesis_task
+# from backend.app.core.db import SessionLocal
 
 i_corp_theme()  
 apply_css()       
@@ -40,33 +41,34 @@ with container:
                 
                 #If there are no missing fields then the evaluate_hypothesis_task will begin 
             else:
+                # db = SessionLocal()
                 #Loading animation because anlysis is not instant 
                 with st.spinner("Analyzing your Hypothesis"):
 
                     #gets the current session that is in right now 
                     current_team = st.session_state.get("current_user", "test_team")
 
-                    try: 
-                        result = evaluate_hypothesis_task(
-                        hypothesis_id=1, 
-                        hypothesis_text=hyp_details, 
-                        hypothesis_type=hyp_selection, 
-                        team_id=current_team
-                        )
+                    # try: 
+                    #     result = evaluate_hypothesis_task(
+                    #     hypothesis_id=1, 
+                    #     hypothesis_text=hyp_details, 
+                    #     hypothesis_type=hyp_selection, 
+                    #     team_id=current_team
+                    #     )
 
 
-                        score = result.get('score', 0) 
-                        feedback_text = result.get('feedback', "No feedback provided.")
-
-                    except Exception as e: 
-                        st.error(f"Backend Error: {e}")
-                        score = 0
-                        feedback_text = "Error occurred."
+                        # score = result.get('score', 0) 
+                        # feedback_text = result.get('feedback', "No feedback provided.")
+                        # st.success("Analysis Complete!")
 
 
-                    time.sleep(2.5)
-                    st.success("Analysis Complete!")
-            
+                    # except Exception as e: 
+                    #     st.error(f"Backend Error: {e}")
+                    #     score = 0
+                    #     feedback_text = "Error occurred."
+                    # finnaly:
+                    # # db.close()
+                                
             #have the score show colors on a wheel like red if its low yellow if its like 65-79 and light green then green then dark green for 80-89 90-99 and 100
             if score >= 80:
                 st.success("You have a Strong Hypothesis")
